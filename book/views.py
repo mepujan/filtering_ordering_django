ca00c0f34687e58a19f01c27137ae2cca97c0c75
Pujan Gautam <mepujan10@gmail.com>
2023-11-21 13:29:47 -0500

    added ordering filters

--- book/views.py
+++ book/views.py
@@ -4,12 +4,14 @@ from .models import Book
 from .serializers import BookSerializers
 from .pagination import BookPagination
 from .filters import PublicationFIlterRange
+from rest_framework.filters import OrderingFilter
 
 
 class BookListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
     pagination_class = BookPagination
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['title', 'author']
     filterset_class = PublicationFIlterRange
+    ordering_fields = ['title', 'author', 'publication']
